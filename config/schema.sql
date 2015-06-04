606ac86020be3dbfc6b728f573bbc7769a89f342
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-06-04 11:47:40 +0900

    update schema

--- config/schema.sql
+++ config/schema.sql
@@ -1,8 +1,8 @@
 create table `bot_accounts` (
   `id`                  int(10)      unsigned not null,
   `account_id`          int(10)      unsigned not null,
-  `keyword_en`          bigint(20)   unsigned not null,
-  `account_screen_name` bigint(20)   unsigned,
+  `keyword_en`          varchar(255) unsigned not null,
+  `account_screen_name` varchar(255) unsigned,
   `consumer_key`        varchar(255) not null,
   `consumer_secret`     varchar(255) not null,
   `access_token`        varchar(255) not null,
@@ -12,9 +12,16 @@ create table `bot_accounts` (
 ) engine=InnoDB default charset=utf8;
 
 create table `keywords` (
-  `id`         int(10)    unsigned not null,
-  `keyword_en` bigint(20) unsigned not null,
-  `keyword_ja` bigint(20) unsigned not null,
+  `id`         int(10)      unsigned not null,
+  `keyword_en` varchar(255) unsigned not null,
+  `keyword_ja` varchar(255) unsigned not null,
   primary key(`id`),
   index(`keyword_en`)
+) engine=InnoDB default charset=utf8;
+
+create table `seed_accounts` (
+  `id`                  int(10)      unsigned not null,
+  `account_screen_name` varchar(255) not null,
+  `account_id`          bigint(20)   unsigned,
+  primary key(`id`)
 ) engine=InnoDB default charset=utf8;
