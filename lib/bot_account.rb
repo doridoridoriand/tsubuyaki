bc01fa7a3b2ce8874def494463988fcd436104d6
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-06-04 22:23:49 +0900

    [WIP] connecting twitter api

--- lib/bot_account.rb
+++ lib/bot_account.rb
@@ -1,20 +1,2 @@
-require 'twitter'
-
 class BotAccount < ActiveRecord::Base
-  def api_client
-    @api_client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key        = consumer_key
-      config.consumer_secret     = consumer_secret
-      config.access_token        = access_token
-      config.access_token_secret = access_token_secret
-    end
-  end
-
-  def friend_ids
-    api_client.friend_ids.to_a
-  end
-
-  def follower_ids
-    api_client.follower_ids.to_a
-  end
 end
