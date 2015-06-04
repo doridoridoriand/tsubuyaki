b76b9a095332904c89c6aef70c4d4dd1eb44375b
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-06-04 12:49:14 +0900

    trivial fix

--- config/schema.sql
+++ config/schema.sql
@@ -1,22 +1,20 @@
 create table `bot_accounts` (
   `id`                  int(10)      unsigned not null,
-  `account_id`          int(10)      unsigned not null,
-  `keyword_en`          varchar(255) unsigned not null,
-  `account_screen_name` varchar(255) unsigned,
+  `account_id`          bigint(20)   unsigned not null,
+  `keyword_en`          varchar(255) not null,
+  `account_screen_name` varchar(255) ,
   `consumer_key`        varchar(255) not null,
   `consumer_secret`     varchar(255) not null,
   `access_token`        varchar(255) not null,
   `access_token_secret` varchar(255) not null,
-  primary key(`id`),
-  index(`keyword_en`)
+  primary key(`id`)
 ) engine=InnoDB default charset=utf8;
 
 create table `keywords` (
   `id`         int(10)      unsigned not null,
-  `keyword_en` varchar(255) unsigned not null,
-  `keyword_ja` varchar(255) unsigned not null,
-  primary key(`id`),
-  index(`keyword_en`)
+  `keyword_en` varchar(255) not null,
+  `keyword_ja` varchar(255) not null,
+  primary key(`id`)
 ) engine=InnoDB default charset=utf8;
 
 create table `seed_accounts` (
