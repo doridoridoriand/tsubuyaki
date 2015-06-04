6d2711c8614f498bea59db03e4ae2e7f15323ef0
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-06-04 19:49:32 +0900

    [WIP] add helper function to generate tweet content

--- helper/tweet_specific_keyword_helper.rb
+++ helper/tweet_specific_keyword_helper.rb
@@ -2,7 +2,15 @@ module TweetSpecificKeywordHelper
   def generate_tweet_content
     data = []
     self.each do |entry|
-      data << "#{entry['title']}#{entry['description']}#{entry['favrica_url']}"
+      data << "【#{entry['title']}】#{entry['description']}#{entry['favrica_url']}"
+    end
+    data
+  end
+
+  def generate_tweet_content_with_direct_link
+    data = []
+    self.each do |entry|
+      data << "【#{entry['title']}】#{entry['description']}#{entry['stocks'][0]['item_url']}"
     end
     data
   end
