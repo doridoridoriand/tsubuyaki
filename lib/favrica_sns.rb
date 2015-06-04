ba4deb9b1da6079b929acc6719d24df2dcbf0b4a
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-06-04 16:52:04 +0900

    add gems and add URL endpoint

--- lib/favrica_sns.rb
+++ lib/favrica_sns.rb
@@ -1,12 +1,18 @@
 require 'active_record'
+require 'cache_local'
 require 'yaml'
+require 'optparse'
+require 'logger'
+require 'json'
+require 'open-uri'
 require 'bot_account'
 require 'seed_account'
-require 'keyword'
+require 'favrica_keyword'
 
 db_config = YAML.load_file(File.expand_path(File.join(__FILE__, '..', '..', 'config', 'database.yml')))
 ActiveRecord::Base.establish_connection(db_config['db']['production'])
 
-module LocalNews
+module FavricaSns
   CACHE_DIR = File.expand_path(File.join(__FILE__, '..', '..', 'tmp', 'cache'))
+  FAVRICA_SEARCH_URL_ENDPOINT = 'https://favrica.net/api/v3/items/search?text='
 end
