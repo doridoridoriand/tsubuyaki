a2f4409a0b26008d047275e22a4f30a3fb90458a
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-06-05 10:24:03 +0900

    trivial fix

--- helper/tweet_specific_keyword_helper.rb
+++ helper/tweet_specific_keyword_helper.rb
@@ -13,7 +13,7 @@ module TweetSpecificKeywordHelper
   def generate_tweet_content_with_direct_link
     data = []
     self.each do |entry|
-      data << "【#{entry['title']}】#{entry['description']} #{entry['stocks'][0]['item_url']}"
+      data << "【#{entry['title']}】#{truncate_description(entry['description'])} #{entry['stocks'][0]['item_url']}"
     end
     data
   end
