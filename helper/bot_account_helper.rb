4e216127165bda4fd84c90d1ead79da9a3b8f1c7
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-06-09 20:52:47 +0900

    fix helper methods

--- helper/bot_account_helper.rb
+++ helper/bot_account_helper.rb
@@ -3,23 +3,4 @@ require 'favrica_sns'
 require 'twitter'
 
 module BotAccountModule
-  def api_client
-    self.each do |data|
-      @api_client = Twitter::REST::Client.new do |config|
-        config.consumer_key        = data['consumer_key']
-        config.consumer_secret     = data['consumer_secret']
-        config.access_token        = data['access_token']
-        config.access_token_secret = data['access_token_secret']
-      end
-    end
-    @api_client
-  end
-
-  def friend_ids
-    api_client.friend_ids.to_a
-  end
-
-  def follower_ids
-    api_client.follower_ids.to_a
-  end
 end
