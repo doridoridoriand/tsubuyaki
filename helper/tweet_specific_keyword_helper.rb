3abbc2dc9e3f2a6a415d4e23d9ca57f6879726d8
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-06-09 15:08:23 +0900

    add helper function to parse item keywords

--- helper/tweet_specific_keyword_helper.rb
+++ helper/tweet_specific_keyword_helper.rb
@@ -2,20 +2,23 @@ $: << File.expand_path(File.join(__FILE__, '..', '..', 'lib'))
 require 'favrica_sns'
 
 module TweetSpecificKeywordHelper
-  def generate_tweet_content
+  # TODO:もうちょっと関数の再利用性を高める。てか汚えな
+  def tweet_content
     hash = Hash.new {|h, e| h[e] = {}}
     self.each_with_index do |entry, i|
       hash[i][:tweet_string] = "【#{entry['title']}】#{truncate_description(entry['description'])} #{entry['favrica_url']}"
-      hash[i][:image] = entry['images'].map {|image| image['large_url']}.to_a
+      hash[i][:image] = entry['images'].map {|image| image['large_url']}.to_a.uniq
+      hash[i][:item_keywords] = item_keywords(entry['id'])
     end
     hash
   end
 
-  def generate_tweet_content_with_direct_link
+  def tweet_content_with_direct_link
     hash = Hash.new {|h, e| h[e] = {}}
     self.each_with_index do |entry, i|
       hash[i][:tweet_string] = "【#{entry['title']}】#{truncate_description(entry['description'])} #{entry['stocks'][0]['item_url']}"
-      hash[i][:image] = entry['images'].map {|image| image['large_url']}.to_a
+      hash[i][:image] = entry['images'].map {|image| image['large_url']}.to_a.uniq
+      hash[i][:item_keywords] = item_keywords(entry['id'])
     end
     hash
   end
@@ -24,5 +27,9 @@ module TweetSpecificKeywordHelper
 
   def truncate_description(string)
     string[FavricaSns::DESCRIPTION_TRUNCATE[0]..FavricaSns::DESCRIPTION_TRUNCATE[1]]
+  end
+
+  def item_keywords(item_id)
+    JSON.parse!(open(URI.encode("#{FavricaSns::FAVRICA_ITEM_ENDPOINT}#{item_id}/keywords")).read).map {|node| node['value']}.to_a
   end
 end
