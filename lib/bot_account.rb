564c2e10359856877e0edd9b2986d64d12c8cdb1
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-06-10 19:51:09 +0900

    [WIP] refactoring

--- lib/bot_account.rb
+++ lib/bot_account.rb
@@ -20,4 +20,35 @@ class BotAccount < ActiveRecord::Base
   def follower_ids
     api_client.follower_ids.to_a
   end
+
+  def tweet_content
+    hash = Hash.new {|h, e| h[e] = {}}
+    json_data.each_with_index do |entry, i|
+      hash[i][:tweet_string] = "【#{entry['title']}】#{truncate_description(entry['description'])} #{entry['favrica_url']}"
+      hash[i][:image] = entry['images'].map {|image| image['large_url']}.to_a.uniq
+      hash[i][:item_keywords] = item_keywords(entry['id'])
+    end
+    hash
+  end
+
+  def hash_tags(source_keyword, item_keywords)
+    item_keywords.delete(source_keyword)
+    hash_keywords = item_keywords.map {|keyword| keyword = "##{keyword}"}
+    if (hash_keywords.count > 1)
+      return_array_data = hash_keywords.join(' ')
+    else
+      return_array_data = hash_keywords[0]
+    end
+    return_array_data
+  end
+
+  private
+
+  def truncate_description(string)
+    string[FavricaSns::DESCRIPTION_TRUNCATE[0]..FavricaSns::DESCRIPTION_TRUNCATE[1]] << '…'
+  end
+
+  def item_keywords(item_id)
+    JSON.parse!(open(URI.encode("#{FavricaSns::FAVRICA_ITEM_ENDPOINT}#{item_id}/keywords")).read).map {|node| node['value']}.to_a
+  end
 end
