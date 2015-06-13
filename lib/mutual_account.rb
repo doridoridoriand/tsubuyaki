f708ce04e4a22835e080db1379320ed2d8c76646
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-06-13 15:42:32 +0900

    [WIP] creating follow_friend_of_mutual_account

---
+++ lib/mutual_account.rb
@@ -0,0 +1,10 @@
+class Mutual_Account < ActiveRecord::Base
+  def api_client
+    @api_client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key        = consumer_key
+      config.consumer_secret     = consumer_secret
+      config.access_token        = access_token
+      config.access_token_secret = access_token_secret
+    end
+  end
+end
