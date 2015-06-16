8144e590abfa6858d46353c89f2d637e9c8f0b6b
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-06-16 20:57:41 +0900

    update scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,9 +3,15 @@ set :environment, :production
 env :PATH, ENV['PATH']
 job_type :rbenv_rake, %q!eval "$(rbenv init -)"; cd :path && :environment_variable=:environment bundle exec rake :task --silent :output!
 
-#every :hour do
-#  command "/home/vagrant/works/tsubuyaki/bin/tweet_specific_keyword -k tshirt -n 3 -l /home/vagrant/works/tsubuyaki/log/tweet_specific_keyword"
-#end
+# 5分毎に行うタスク用
+time = []
+(0..23).to_a.each do |hour|
+  (0..59).to_a.each do |minute|
+    if minute % 5 == 0
+      time << "#{'%02d' % hour}:#{'%02d' % minute}"
+    end
+  end
+end
 
 # 現状ではあまり:hourと代わり映えしないけど、細かく指定できる方がいいのでこちらで登録。あと夜つぶやいても仕方がない気がするので
 # これだとただのスパムボットに成り下がっているので一旦すべて廃止
@@ -50,7 +56,14 @@ every 1.day, at: "18:25" do
   command "/home/dorian/Works/tsubuyaki/bin/tweet_specific_keyword -k outdoor -n 3 -l /home/dorian/Works/tsubuyaki/log/tweet_specific_keyword -e"
 end
 
-#every 1.minute do
-#  command "/home/vagrant/works/tsubuyaki/bin/simple_tweet -k pumps"
-#end
+# follow follower of seed account
+every :monday, at: time  do
+end
 
+# follow mutual accounts
+every :wednesday, at: time  do
+end
+
+# unfollow not followback account
+every :saturday, at: time  do
+end
