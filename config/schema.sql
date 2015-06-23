51e5492b99e946563f9367bfa6b2a8c4fc25385b
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-06-23 13:25:46 +0900

    add new tables

--- config/schema.sql
+++ config/schema.sql
@@ -31,3 +31,21 @@ create table `bot_account_follow_followers` (
   `follower`   bigint(20)   unsigned,
   primary key(`id`)
 ) engine=InnoDB default charset=utf8;
+
+# まだ使うかは保留
+create table `bot_rate_limits` (
+  `id`         int(10)      unsigned not null,
+  `keyword_en` varchar(255) not null,
+  `remaining`  int(10)      unsigned,
+  `reset_at`   int(10)      unsigned, # APIからの返り値がエポックタイム
+  primary key(`id`)
+) engine=InnoDB default charset=utf8;
+
+create table 'bot_followers' (
+  `id`                  int(10)      unsigned not null,
+  `keyword_en`          varchar(255) not null,
+  `bot_account_id`      bigint(20)   unsigned,
+  `account_id`          bigint(20)   unsigned,
+  `account_screen_name` varchar(255),
+  primary key(`id`)
+) engine=InnoDB default charset=utf8;
