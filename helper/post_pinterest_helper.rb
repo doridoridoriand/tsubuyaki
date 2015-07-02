1fcc931fd299faa1bd1ad40cf58e4210436d7dc7
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-07-02 18:05:49 +0900

    add function to save screenshots

--- helper/post_pinterest_helper.rb
+++ helper/post_pinterest_helper.rb
@@ -3,7 +3,7 @@ require 'favrica_sns'
 
 module PostPinterestHelper
 
-  def post_pinterest
+  def post_pinterest(target_keyword)
     headless = Headless.new
     headless.start
     driver = Selenium::WebDriver.for :chrome
@@ -11,44 +11,43 @@ module PostPinterestHelper
     p pinterest_url(self[:favrica_url], self[:image].sample, self[:tweet_description_no_favrica_url])
     driver.navigate.to pinterest_url(self[:favrica_url], self[:image].sample, self[:tweet_description_no_favrica_url])
 
-    driver.save_screenshot('/home/vagrant/dev/unko0.png')
+    driver.save_screenshot("#{FavricaSns::SCREENSHOT_SAVE_PLACE}#{target_keyword}_#{Time.now.to_s}.png")
 
     driver.find_element(:xpath, "//div[@class='savePinDialog']/div/div/a").click
 
-    driver.save_screenshot('/home/vagrant/dev/unko1.png')
+    driver.save_screenshot("#{FavricaSns::SCREENSHOT_SAVE_PLACE}#{target_keyword}_#{Time.now.to_s}.png")
     sleep(5)
-    driver.save_screenshot('/home/vagrant/dev/unko1_1.png')
+    driver.save_screenshot("#{FavricaSns::SCREENSHOT_SAVE_PLACE}#{target_keyword}_#{Time.now.to_s}.png")
     sleep(5)
-    driver.save_screenshot('/home/vagrant/dev/unko1_2.png')
+    driver.save_screenshot("#{FavricaSns::SCREENSHOT_SAVE_PLACE}#{target_keyword}_#{Time.now.to_s}.png")
 
     username = driver.find_element(:xpath, "//li[@class='loginUsername']/input")
     username.send_keys(FavricaSns::PINTEREST_ID)
 
-    driver.save_screenshot('/home/vagrant/dev/unko2.png')
+    driver.save_screenshot("#{FavricaSns::SCREENSHOT_SAVE_PLACE}#{target_keyword}_#{Time.now.to_s}.png")
 
     password = driver.find_element(:xpath, "//li[@class='loginPassword']/input")
     password.send_keys(FavricaSns::PINTEREST_PASS)
 
-    driver.save_screenshot('/home/vagrant/dev/unko3.png')
+    driver.save_screenshot("#{FavricaSns::SCREENSHOT_SAVE_PLACE}#{target_keyword}_#{Time.now.to_s}.png")
 
-    driver.save_screenshot('/home/vagrant/dev/unko4.png')
+    driver.save_screenshot("#{FavricaSns::SCREENSHOT_SAVE_PLACE}#{target_keyword}_#{Time.now.to_s}.png")
 
     driver.find_element(:xpath, "//div[@class='formFooterButtons']/button").click
 
-    driver.save_screenshot('/home/vagrant/dev/unko5.png')
+    driver.save_screenshot("#{FavricaSns::SCREENSHOT_SAVE_PLACE}#{target_keyword}_#{Time.now.to_s}.png")
     sleep(5)
-    driver.save_screenshot('/home/vagrant/dev/unko6.png')
+    driver.save_screenshot("#{FavricaSns::SCREENSHOT_SAVE_PLACE}#{target_keyword}_#{Time.now.to_s}.png")
     sleep(5)
-    driver.save_screenshot('/home/vagrant/dev/unko7.png')
-    sleep(5)
+    driver.save_screenshot("#{FavricaSns::SCREENSHOT_SAVE_PLACE}#{target_keyword}_#{Time.now.to_s}.png")
 
     driver.find_element(:xpath, "//ul[@class='sectionItems']/li/div/button").click
 
-    driver.save_screenshot('/home/vagrant/dev/unko8.png')
+    driver.save_screenshot("#{FavricaSns::SCREENSHOT_SAVE_PLACE}#{target_keyword}_#{Time.now.to_s}.png")
     sleep(5)
-    driver.save_screenshot('/home/vagrant/dev/unko9.png')
+    driver.save_screenshot("#{FavricaSns::SCREENSHOT_SAVE_PLACE}#{target_keyword}_#{Time.now.to_s}.png")
     sleep(5)
-    driver.save_screenshot('/home/vagrant/dev/unko10.png')
+    driver.save_screenshot("#{FavricaSns::SCREENSHOT_SAVE_PLACE}#{target_keyword}_#{Time.now.to_s}.png")
   end
 
   private
